dacd6fb72a181cf2a08911f21142921568221068
Rajat Kumar <rajat9258@gmail.com>
2024-02-21 00:17:19 +0530

    comment add

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,28 +1,39 @@
-import jwt from "jsonwebtoken"
-import { asyncHandler } from "../utils/asyncHandler.js"
-import { ApiError } from "../utils/ApiError.js"
-import { User } from "../models/user.model.js"
+import jwt from "jsonwebtoken";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
 
+// Middleware function to verify JWT token
 export const verifyJWT = asyncHandler(async (req, _, next) => {
   try {
+    // Retrieve JWT token from cookies or Authorization header
     const token =
       req.cookies?.accessToken ||
       req.header("Authorization")?.replace("Bearer ", "");
 
-    // console.log(token);
+    // If token is not provided, throw Unauthorized error
     if (!token) {
       throw new ApiError(401, "Unauthorized request");
     }
+
+    // Verify the JWT token using the access token secret
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    // Find user by decoded token's _id and exclude sensitive information
     const user = await User.findById(decodedToken?._id).select("-password -refreshToken");
 
+    // If user is not found, throw Invalid Access Token error
     if (!user) {
       throw new ApiError(401, "Invalid Access Token");
     }
 
+    // Attach user object to request for further processing
     req.user = user;
+
+    // Call next middleware
     next();
   } catch (error) {
+    // If any error occurs during token verification, throw error with appropriate message
     throw new ApiError(401, error?.message || "Invalid access token");
   }
 });
--- src/middlewares/multer.middlware.js
+++ src/middlewares/multer.middlware.js
@@ -1,15 +1,18 @@
 import multer from "multer";
 
+// Define storage configuration for multer
 const storage = multer.diskStorage({
+    // Set destination directory for storing uploaded files
     destination: function (req, file, cb) {
-      cb(null, "./public/temp")
+        cb(null, "./public/temp");
     },
+    // Set filename for uploaded files
     filename: function (req, file, cb) {
-      
-      cb(null, file.originalname)
+        cb(null, file.originalname);
     }
-  })
-  
+});
+
+// Create multer upload middleware with defined storage configuration
 export const upload = multer({ 
     storage, 
-})
+});
