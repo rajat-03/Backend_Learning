3b92caf8adcce3f5611b09972f82d01dc1613995
Rajat Kumar <rajat9258@gmail.com>
2024-02-22 01:31:13 +0530

    add all remaining controllers

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,20 +1,75 @@
-import mongoose from "mongoose"
-import {Video} from "../models/video.model.js"
-import {Subscription} from "../models/subscription.model.js"
-import {Like} from "../models/like.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import mongoose from "mongoose";
+import { Video } from "../models/video.model.js";
+import { Subscription } from "../models/subscription.model.js";
+import { Like } from "../models/like.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const getChannelStats = asyncHandler(async (req, res) => {
-    // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
-})
+  // Get the channel ID from the logged-in user
+  const channelId = req.user._id;
+
+  try {
+    // Get total video views
+    const totalViews = await Video.aggregate([
+      {
+        $match: { owner: mongoose.Types.ObjectId(channelId) },
+      },
+      {
+        $group: {
+          _id: null,
+          totalViews: { $sum: "$views" },
+        },
+      },
+    ]);
+
+    // Get total subscribers
+    const totalSubscribers = await Subscription.countDocuments({
+      channel: channelId,
+    });
+
+    // Get total videos
+    const totalVideos = await Video.countDocuments({ owner: channelId });
+
+    // Get total likes on videos
+    const totalLikes = await Like.countDocuments({
+      video: { $in: await Video.find({ owner: channelId }).distinct("_id") },
+    });
+
+    // Prepare the response
+    const channelStats = {
+      totalViews: totalViews.length ? totalViews[0].totalViews : 0,
+      totalSubscribers,
+      totalVideos,
+      totalLikes,
+    };
+
+    // Send the response
+    res.status(200).json(new ApiResponse(200, channelStats, "Channel stats retrieved successfully"));
+  } catch (error) {
+    // Handle errors
+    throw new ApiError(500, "Internal Server Error");
+  }
+});
 
 const getChannelVideos = asyncHandler(async (req, res) => {
-    // TODO: Get all the videos uploaded by the channel
-})
+  // Get the channel ID from the logged-in user
+  const channelId = req.user._id;
+
+  try {
+    // Get all videos uploaded by the channel
+    const videos = await Video.find({ owner: channelId });
+
+    // Send the response
+    res.status(200).json(new ApiResponse(200, videos, "Channel videos retrieved successfully"));
+  } catch (error) {
+    // Handle errors
+    throw new ApiError(500, "Internal Server Error");
+  }
+});
 
 export {
-    getChannelStats, 
-    getChannelVideos
-    }
+  getChannelStats,
+  getChannelVideos
+};
--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -1,33 +1,78 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import {Like} from "../models/like.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import mongoose from "mongoose";
+import { Like } from "../models/like.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
-    const {videoId} = req.params
-    //TODO: toggle like on video
-})
+  const { videoId } = req.params;
+  const { userId } = req.user; // Assuming userId is available in req.user
+
+  // Check if the user has already liked the video
+  const existingLike = await Like.findOne({ video: videoId, likedBy: userId });
+
+  if (existingLike) {
+    // User has already liked the video, so unlike it
+    await Like.deleteOne({ _id: existingLike._id });
+    return res.status(200).json(new ApiResponse(200, {}, "Video unliked successfully"));
+  } else {
+    // User has not liked the video, so like it
+    const newLike = new Like({ video: videoId, likedBy: userId });
+    await newLike.save();
+    return res.status(200).json(new ApiResponse(200, {}, "Video liked successfully"));
+  }
+});
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
-    const {commentId} = req.params
-    //TODO: toggle like on comment
+  const { commentId } = req.params;
+  const { userId } = req.user; // Assuming userId is available in req.user
 
-})
+  // Check if the user has already liked the comment
+  const existingLike = await Like.findOne({ comment: commentId, likedBy: userId });
+
+  if (existingLike) {
+    // User has already liked the comment, so unlike it
+    await Like.deleteOne({ _id: existingLike._id });
+    return res.status(200).json(new ApiResponse(200, {}, "Comment unliked successfully"));
+  } else {
+    // User has not liked the comment, so like it
+    const newLike = new Like({ comment: commentId, likedBy: userId });
+    await newLike.save();
+    return res.status(200).json(new ApiResponse(200, {}, "Comment liked successfully"));
+  }
+});
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
-    const {tweetId} = req.params
-    //TODO: toggle like on tweet
-}
-)
+  const { tweetId } = req.params;
+  const { userId } = req.user; // Assuming userId is available in req.user
+
+  // Check if the user has already liked the tweet
+  const existingLike = await Like.findOne({ tweet: tweetId, likedBy: userId });
+
+  if (existingLike) {
+    // User has already liked the tweet, so unlike it
+    await Like.deleteOne({ _id: existingLike._id });
+    return res.status(200).json(new ApiResponse(200, {}, "Tweet unliked successfully"));
+  } else {
+    // User has not liked the tweet, so like it
+    const newLike = new Like({ tweet: tweetId, likedBy: userId });
+    await newLike.save();
+    return res.status(200).json(new ApiResponse(200, {}, "Tweet liked successfully"));
+  }
+});
 
 const getLikedVideos = asyncHandler(async (req, res) => {
-    //TODO: get all liked videos
-})
+  const { userId } = req.user; // Assuming userId is available in req.user
+
+  // Find all liked videos by the user
+  const likedVideos = await Like.find({ likedBy: userId }).populate("video");
+
+  return res.status(200).json(new ApiResponse(200, likedVideos, "Liked videos fetched successfully"));
+});
 
 export {
-    toggleCommentLike,
-    toggleTweetLike,
-    toggleVideoLike,
-    getLikedVideos
-}
+  toggleCommentLike,
+  toggleTweetLike,
+  toggleVideoLike,
+  getLikedVideos
+};
